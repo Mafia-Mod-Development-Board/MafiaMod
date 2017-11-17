3a51a87d617a317a29624b4306ab3276556addb0
Arthod <Arthodgdt@gmail.com>
2017-11-18 00:16:58 +0100

    Added Latest Version

---
+++ organizedcrime/common/defines/00_defines.lua
@@ -0,0 +1,55 @@
+-- LIFESPAN
+NDefines.NCharacter.AGE_OLD = 60									-- Non-child characters below this age have the 'adult' static modifier applied
+NDefines.NCharacter.AGE_VERY_OLD = 80								-- Non-child characters below this age have the 'old' static modifier applied
+NDefines.NCharacter.MAX_CHILD_BIRTH_AGE = 51						-- Female menopause age.
+NDefines.NCharacter.NATURAL_DEATH_CHANCE_AGE_50 = 75				-- Natural deaths per decade out of 10000 people: Age 50-59
+NDefines.NCharacter.NATURAL_DEATH_CHANCE_AGE_60 = 250				-- Natural deaths per decade out of 10000 people: Age 60-69
+NDefines.NCharacter.NATURAL_DEATH_CHANCE_AGE_70 = 1000				-- Natural deaths per decade out of 10000 people: Age 70-79
+NDefines.NCharacter.NATURAL_DEATH_CHANCE_AGE_80 = 2500				-- Natural deaths per decade out of 10000 people: Age 80-90
+NDefines.NCharacter.NATURAL_DEATH_CHANCE_AGE_90 = 6000				-- Natural deaths per decade out of 10000 people: Age 90-99
+NDefines.NCharacter.NATURAL_DEATH_CHANCE_AGE_100 = 8000				-- Natural deaths per decade out of 10000 people: Age 100+
+--
+-- DYNASTIC SCOREBOARD
+NDefines.NEndGame.DYN1_SCORE = 100000
+NDefines.NEndGame.DYN1_ID = 100
+NDefines.NEndGame.DYN2_SCORE = 90000
+NDefines.NEndGame.DYN2_ID = 1001
+NDefines.NEndGame.DYN3_SCORE = 80000
+NDefines.NEndGame.DYN3_ID = 1000
+NDefines.NEndGame.DYN4_SCORE = 70000
+NDefines.NEndGame.DYN4_ID = 2000
+NDefines.NEndGame.DYN5_SCORE = 60000
+NDefines.NEndGame.DYN5_ID = 2001
+NDefines.NEndGame.DYN6_SCORE = 50000
+NDefines.NEndGame.DYN6_ID = 6000
+NDefines.NEndGame.DYN7_SCORE = 40000
+NDefines.NEndGame.DYN7_ID = 6001
+NDefines.NEndGame.DYN8_SCORE = 30000
+NDefines.NEndGame.DYN8_ID = 5000
+NDefines.NEndGame.DYN9_SCORE = 20000
+NDefines.NEndGame.DYN9_ID = 3000
+NDefines.NEndGame.DYN10_SCORE = 15000
+NDefines.NEndGame.DYN10_ID = 3001
+NDefines.NEndGame.DYN11_SCORE = 10000
+NDefines.NEndGame.DYN11_ID = 4000
+NDefines.NEndGame.DYN12_SCORE = 7500
+NDefines.NEndGame.DYN12_ID = 4001
+NDefines.NEndGame.DYN13_SCORE = 5000
+NDefines.NEndGame.DYN13_ID = 2021
+NDefines.NEndGame.DYN14_SCORE = 2000
+NDefines.NEndGame.DYN14_ID = 1009
+NDefines.NEndGame.DYN15_SCORE = 1000
+NDefines.NEndGame.DYN15_ID = 4017
+--
+-- MILITARY
+NDefines.NMilitary.USE_AUTOMATIC_CALL_TO_ARMS = 1							-- Whether to automatically call all your allies at the beginning of a war or not
+NDefines.NMilitary.AUTOMATICALLY_ACCEPTS_CALL_TO_ARMS = 1					-- Whether allies (including players) automatically and instantly accept calls to arms or if they can refuse
+--
+-- TITLES
+NDefines.NTitle.ALLOW_DE_JURE_ASSIMILATION_ANYWHERE = 0	-- If set to 0, will only assimilate duchy titles which contain the assimilating Kingdom's capital or border existing de jure land of that title
+NDefines.NTitle.MILITARY_GOV_CREATE_KINGDOMS = 1		-- If Mercenaries and holy orders are allowed to create kingdom titles
+
+-- ARMY
+NDefines.NMilitary.ARMY_MOVEMENT_SPEED = 9
+NDefines.NMilitary.NAVY_MOVEMENT_SPEED = 45
+-- NAVAL
